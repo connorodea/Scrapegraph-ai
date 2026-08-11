2281ba61a0932d6544383758414c3a50daff8e6b
Connor O'Dea <102129457+connorodea@users.noreply.github.com>
2026-08-11 03:51:03 -0600

    feat(radar): add package exports

---
+++ applications/juricratic_research_radar/juricratic_radar/__init__.py
@@ -0,0 +1,25 @@
+"""Juricratic Research Radar package."""
+
+from .models import (
+    AccessDecision,
+    ArtifactExtraction,
+    CandidateKind,
+    QueryPlan,
+    ResearchCandidate,
+    RunEvent,
+    RunKind,
+    RunSummary,
+    ScoreBreakdown,
+)
+
+__all__ = [
+    "AccessDecision",
+    "ArtifactExtraction",
+    "CandidateKind",
+    "QueryPlan",
+    "ResearchCandidate",
+    "RunEvent",
+    "RunKind",
+    "RunSummary",
+    "ScoreBreakdown",
+]
